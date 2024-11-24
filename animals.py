1055ee373c8ca6ce8fac066b3ba2bd7d8ebc8edc
Soshi Sakamoto <s23C1061BH@s.chibakoudai.jp>
2024-11-24 23:55:37 +0900

    Add new command

---
+++ animals.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# SPDX-FileCopyrightText: 2024 Soshi Sakamoto
+# SPDX-License-Identifier: BSD-3-Clause
+
+animals = [ "kangaroo", "penguin", "hippopotamus" ]
+print("1匹目: " + animals[0])
+print("2匹目: " + animals[1])
+print("3匹目: " + animals[-1])
