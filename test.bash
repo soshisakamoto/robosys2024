22e9a81eca47140bca85ed28b9732b375b56fe6f
Soshi Sakamoto <s23C1061BH@s.chibakoudai.jp>
2024-11-24 19:17:17 +0900

    Improve the test

--- test.bash
+++ test.bash
@@ -1,5 +1,7 @@
-#!/bin/bash
-# spdx-FileCopyrightText: 2024 Soshi Sakamoto
+#!/bin/bash 
+# SPDX-FileCopyrightText: 2024 Soshi Sakamoto
+# SPDX-License-Identifier: BSD-3-Clause
+
 ng() {
         echo ${1}行目が違うよ
         res=1
@@ -7,6 +9,7 @@ ng() {
 
 res=0
 
+### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng "$LINENO"
 
